ffcd5a274c7bb34d03ff2d7e99b6b2baac3eccb0
Farhan Khan <thefarhanxkhan@gmail.com>
2026-08-16 00:48:47 +0500

    GTM added

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Footer } from '@/components/footer'
 import { CartDrawer } from '@/components/cart-drawer'
 import { WhatsAppButton } from '@/components/whatsapp-button'
 import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ 
@@ -35,10 +36,29 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
   return (
     <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`} suppressHydrationWarning>
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+      </head>
       <body className="font-sans antialiased">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <CartProvider>
           <Header />
           {children}
@@ -49,7 +69,6 @@ export default function RootLayout({
         <Analytics />
         <SpeedInsights />
       </body>
-      {gtmId && <GoogleTagManager gtmId={gtmId} />}
     </html>
   )
 }
